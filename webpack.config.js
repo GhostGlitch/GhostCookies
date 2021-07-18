192a963e56a64a8e8d268597c07c713a36ee88c9
GhostGlitch <taylerobin@gmail.com>
2021-07-18 13:44:00 -0600

    Fixed webpack entry

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require("path");
 const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
-  entry: "./src/index.js",
+  entry: "./src/GhostCookies.js",
   output: {
     path: path.resolve(__dirname, "dist"),
   },
